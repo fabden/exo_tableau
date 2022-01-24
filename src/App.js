def9ae660a1878a78d0ccbff0d929a500279f398
FabDen <fabden@gmail.com>
2022-01-24 18:27:56 +0100

    structure de base exo tri tableau

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom du film</th>
+            <th>Réalisateur</th>
+            <th>Année de production</th>
+            <th>Nationalité </th>
+            <th>Dernière diffusion</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+            <td>The table body</td>
+            <td>with two columns</td>
+            <td>with two columns</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
