5bd43bf110bc23efb44418fbfcbf748a23c23450
FabDen <fabden@gmail.com>
2022-01-27 20:52:44 +0100

    formule calcule tp5 ok

--- src/component/TopFive/TopFive.js
+++ src/component/TopFive/TopFive.js
@@ -1,15 +1,72 @@
 import './TopFive.css'; 
-import React, { useState } from 'react';
+import React from 'react';
+import getUniqueValeur from '../../tools/getUniqueVal';
 
-function TopFive({handelOpenModal}) {
+function TopFive({handelOpenModal,datas}) {
+
+//fonction top 5 film 
+const top5Film = (data) =>{
+  const triFromDif = data.sort((a,b)=>(b.nb_diffusion - a.nb_diffusion)).slice(0,5)
+  return triFromDif
+};
+
+//fonction top 5  film meilleur ratio
+
+const top5MeilleurRatio = (data)=>{
+
+const calRatio = data.map((e)=>{ 
+  const cRatio = e.nb_premiere_partie/e.nb_diffusion
+  return {
+    ...e,ratio: cRatio
+  }
+}).sort((a,b)=>(b.ratio - a.ratio)).slice(0,5)
+
+return calRatio
+
+}
+
+//fonction top 5 des realisateurs avec la meilleur moyenne de diffusion
+
+const top5Realisateur = (data)=>{
+
+  const uniqueUser = data.map((item)=>{
+    const diviseItem = item.realisateur.split('/')   
+    return diviseItem
+   }).flat().filter(getUniqueValeur)
+
+  const addMoyendifusionRealisateur = uniqueUser.map((user)=>{
+  const recupRealisaeurPM = data.filter((item)=>item.realisateur.includes(user))
+  const AdditionMoyenneDiffusion = recupRealisaeurPM.reduce((a,b)=>(a+b.moyenne_diffusion_par_an),0)
+    return {realisateur:user,
+            additionMoyenne:AdditionMoyenneDiffusion
+          }
+  })
 
 
-//state statistique film 
+ const moyennefilmRealisateur = addMoyendifusionRealisateur.map((e)=>{
 
-const [statFilm, setStatFilm] = useState("")
+ const moyenne= e.additionMoyenne/datas.length 
+    return{
+      realisateur:e.realisateur,
+      moyenne:moyenne
+    }
+  })
 
-   
 
+const top5realisateurs = moyennefilmRealisateur.sort((a,b)=>(b.moyenne - a.moyenne)).slice(0,5)
+
+//somme des moyens de diffussion par realisateur
+return top5realisateurs
+
+
+
+//nombre de film 
+
+}
+
+console.log(top5Realisateur(datas));
+console.log(top5MeilleurRatio(datas));
+console.log(top5Film(datas));
 
   return (
 
