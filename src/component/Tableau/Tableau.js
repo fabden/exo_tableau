3c9ada46399560347d2f3222bbc4cfa407c7cf2a
FabDen <fabden@gmail.com>
2022-01-28 18:27:16 +0100

    nettoyage dossier

--- src/component/Tableau/Tableau.js
+++ src/component/Tableau/Tableau.js
@@ -27,6 +27,8 @@ function Tableau({ datas }) {
     datas,
   );
 
+  
+
   return (
 
     <table>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// eslint-disable-next-line import/no-named-as-default
+// eslint-disable-next-line import/no-named-as-default-member
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );
 
